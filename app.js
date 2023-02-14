0b8fb8709c100fbe23d5e4dacc85171a5f60faee
Rahul Verma <extreme@Rahuls-MacBook-Air.local>
2023-02-14 11:14:50 +0530

    created express server

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express') // fetching express modules
+const app = express()
+
+app.listen(process.env.PORT || 8000, function(error){
+    if(error) console.log('Error : Launcing Express Server : ', error);
+    else console.log('Success : Launching Express Server');
+})
