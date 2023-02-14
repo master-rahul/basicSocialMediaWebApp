8e685130a5a17b776c80e0e56919fabc422a8a10
Rahul Verma <extreme@Rahuls-MacBook-Air.local>
2023-02-14 11:24:25 +0530

    add directory, create files for config, routes, controllers, views, models

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express') // fetching express modules
 const app = express()
 
 app.listen(process.env.PORT || 8000, function(error){
-    if(error) console.log('Error : Launcing Express Server : ', error);
-    else console.log('Success : Launching Express Server');
+    if(error) console.log('Error : Launcing Express Server : ', error)
+    else console.log('Success : Launching Express Server') 
 })
