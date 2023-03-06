ea3d4d23297f041f22331e6c4f9b4b5c1a040d11
Rahul Verma <extreme@Rahuls-MacBook-Air.local>
2023-03-06 11:31:43 +0530

    update controller queries with async + await

--- controllers/post.js
+++ controllers/post.js
@@ -1,30 +1,47 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
-module.exports.create = function (request, response) {
-    Post.create({
-        content: request.body.content,
-        user: request.user.id
-    }, function (error) {
-        if (error) {
-            console.log('Error Adding Contents : ',error);
-            return response.redirect('back');
-        }
-        else {
-            console.log('Success Adding Contents');
-            return response.redirect('back');
-        }
-    });
+module.exports.create = async function (request, response) {
+    // Post.create({
+    //     content: request.body.content,
+    //     user: request.user.id
+    // }, function (error) {
+    //     if (error) {
+    //         console.log('Error Adding Contents : ',error);
+    //         return response.redirect('back');
+    //     }
+    //     else {
+    //         console.log('Success Adding Contents');
+    //         return response.redirect('back');
+    //     }
+    // });
+    try{
+        let create = await Post.create({ content : request.body.content, user : request.user.id});
+        return response.redirect('back');
+    } catch(error){
+       return response.status('500').send();
+    }
 }
 
 
-module.exports.delete = function (request, response) {
-    Post.findById(request.params.id, function(error, post){
-        // .id means converting the object id to string
-        if(post && post.user == request.user.id){
+module.exports.delete = async function (request, response) {
+    // Post.findById(request.params.id, function(error, post){
+    //     // .id means converting the object id to string
+    //     if(post && post.user == request.user.id){
+    //         post.remove();
+    //         Comment.deleteMany({post : request.params.id}, function (error) {
+    //             return response.redirect('back');
+    //         });
+    //     }else return response.redirect('back');
+    // });
+
+    try{
+        let post = await Post.findById(request.params.id);
+        if (post && post.user == request.user.id) {
             post.remove();
-            Comment.deleteMany({post : request.params.id}, function (error) {
-                return response.redirect('back');
-            });
-        }else return response.redirect('back');
-    });
+            let comment = await Comment.deleteMany({ post: request.params.id });
+        }
+        return response.redirect('back');
+    } catch(error){
+        return response.status(500).send();
+    }
 }
--- controllers/user.js
+++ controllers/user.js
@@ -7,40 +7,77 @@ module.exports.signIn = function (request, response) {
 module.exports.signUp = function (request, response) {
     return response.render('signUp', { title: 'Sign Up' })
 }
-module.exports.add = function (request, response) {
+module.exports.add = async function (request, response) {
     if (request.body.password != request.body.confirm_password) return response.redirect('back');
-    User.findOne({ email: request.email }, function (error, user) {
-        if (error) return response.redirect('back');
-        else if (!user) {
-            User.create({
-                name: request.body.name,
-                email: request.body.email,
-                password: request.body.password
-            },  function (error) {
-                    if (error) return response.redirect('back');
-                    else return response.redirect('/user/signIn');
-            });
-        } else return response.redirect('back');
-    });
+
+    // SECOND BEST_WAY 
+    // User.findOne({ email: request.email }, function (error, user) {
+    //     console.log(request.email);
+    //     console.log(request.body.email);
+    //     if (error) return response.redirect('back');
+    //     else if (!user) {
+    //         User.create({
+    //             name: request.body.name,
+    //             email: request.body.email,
+    //             password: request.body.password
+    //         },  function (error) {
+    //                 if (error) return response.redirect('back');
+    //                 else return response.redirect('/user/signIn');
+    //         });
+    //     } else return response.redirect('back');
+    // });
+    
+    // BEST_WAY 
+    try{
+        let user = await User.findOne({ email: request.body.email });
+        if (user) return response.redirect('back');
+        else {
+            let add = await User.create({ name: request.body.name, email: request.body.email, password: request.body.password });
+            return response.redirect('/user/signIn');
+        }
+    }catch(error){
+            return response.staus('500').send();
+    }
+
 }
 module.exports.remove = function (request, response) {
     return response.render('home', { title : 'Remove' })
 }
-module.exports.update = function (request, response) {
-    if(request.user.id == request.params.id){
-        User.findByIdAndUpdate(request.params.id, {name : request.body.name, email : request.body.email}, function (error, user) {
-            if(error) return response.redirect('back');
-            else return response.redirect('back');
-        });
-    } else return response.status(401).send('Unauthorized');
+module.exports.update = async function (request, response) {
+
+    // SECOND BEST_WAY
+    // if(request.user.id == request.params.id){
+    //     User.findByIdAndUpdate(request.params.id, {name : request.body.name, email : request.body.email}, function (error, user) {
+    //         if(error) return response.redirect('back');
+    //         else return response.redirect('back');
+    //     });
+    // } else return response.status(401).send('Unauthorized');
+
+    // BEST_WAY
+    try{
+        if (request.user.id != request.params.id) return response.status(401).send('Unauthorized');
+        let upd = await User.findByIdAndUpdate(request.params.id,{ name: request.body.name, email: request.body.email });
+        return response.redirect('back');
+    } catch(error){
+        return response.status(401).send('Unauthorized');
+    }
 }
 
-module.exports.profile = function (request, response) {
+module.exports.profile =  async function (request, response) {
+    //SECOND BEST_WAY
     var id = request.params.id;
-    if(id == null) id = request.user.id;
-    User.findById(id, function (error, user) {
-        if(error) return response.redirect('back');
-        else return response.render('profile', {title : 'Profile', profileData: user});
-    });
-  
+    // if(id == null) id = request.user.id;
+    // User.findById(id, function (error, user) {
+    //     if(error) return response.redirect('back');
+    //     else return response.render('profile', {title : 'Profile', profileData: user});
+    // });
+
+    // BEST_WAY
+    try{
+        if(id == null) id = request.user.id;
+        let find = await User.findById(id);
+        return response.render('profile', { title: 'Profile', profileData: find });
+    } catch(error){
+        return response.status(401).send('Unauthorized');
+    }
 }
